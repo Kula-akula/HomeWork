ffc963a541c9e01e5199b9b17d6426c0c72c0050
Kula <1kulanbek@gmail.com>
2019-12-14 18:44:10 +0600

    Home Work #37 second edition

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,28 @@
 package com.company;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Set<Dog> dogs=new HashSet<>();
         for (int i=1; i<=40;i++){
             String name="Dog # ";
             name+=i;
             dogs.add(new Dog(name,i));
+
         }
+        dogs.add(new Dog("Dog # 1", 1));
         System.out.println(dogs);
         System.out.println(dogs.size());
 
 
-        System.out.println("FAQ:\n" +
-                "1. Как вы понимаете hashCode?\n" +
+        FileWriter fileWriter=new FileWriter("FAQ#37_HW.txt");
+        fileWriter.write("1. Как вы понимаете hashCode?\n" +
                 "Это адрес ячейки в которой хранится определенный элемент или несколько одинаковых элементов.\n" +
                 "2. Для чего мы можем переписать метод генерирования hashCode?\n" +
                 "Для того что определять самим при каких схожих параметров считать элементы идентичными и выдавать им одинаковый хешкод.\n" +
@@ -26,7 +31,8 @@ public class Main {
                 "4. Какие отличия есть у HashSet & ArrayList?\n" +
                 "Ну как описывалось в п.3, у него автоматически идет проверка на дубликаты. А ArrayList не проверяя добавляет в свой список.\n" +
                 "5. Как вы думаете, как мы можем избавиться от дубликатов в ArrayList при помощи HashSet?\n" +
-                "Перезапишем Array List при помощи Hash Seta.");
+                "Перезапишем Array List при помощи Hash Set.");
+        fileWriter.close();
     }
 }
 class Dog{
@@ -61,6 +67,20 @@ private int weight;
 
     public void setWeight(int weight) {
         this.weight = weight;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dog dog = (Dog) o;
+        return weight == dog.weight &&
+                name.equals(dog.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, weight);
     }
 
     @Override
